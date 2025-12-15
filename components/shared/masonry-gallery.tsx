e55450c0ec3533fb4ba77c71687030d863fc6a07
Jophkins <sergebrew@gmail.com>
2025-12-14 17:12:29 -0700

    :construction: masonry-gallery component is on test, straight push to 'dev'

---
+++ components/shared/masonry-gallery.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+
+const images = [
+  "https://picsum.photos/id/10/1200/600",
+  "https://picsum.photos/id/29/1400/1200",
+  "https://picsum.photos/seed/529/800/1200",
+  "https://picsum.photos/seed/629/800/1200",
+  "https://picsum.photos/seed/729/800/1200",
+  "https://picsum.photos/seed/829/800/1200",
+  "https://picsum.photos/seed/929/800/1200",
+  "https://picsum.photos/seed/109/800/1200",
+];
+
+function MasonryGallery() {
+  return (
+    <div className="columns-1 sm:columns-2 lg:columns-3 py-10 md:py-20 gap-4">
+      {images.map((src, index) => (
+        <div key={index} className="mb-4 break-inside-avoid">
+          <Image src={src} alt="tst" width="400" height="400" className="w-full object-cover rounded-lg" />
+        </div>
+      ))}
+    </div>
+  );
+}
+export default MasonryGallery;
--- lib/mock-pics.ts
+++ lib/mock-pics.ts
@@ -6,6 +6,6 @@ export type Picture = {
 
 export const MOCK_PICTURES: Picture[] = Array.from({ length: 100 }).map((_, i) => ({
   id: `pic-${i}`,
-  url: `https://picsum.photos/seed/${i}/800/1200`,
+  url: `https://picsum.photos/id/${i + 10}/${i % 3 === 0 ? "800" : "1600"}/${i % 2 === 0 ? "1600" : "600"}`,
   alt: `Random picture ${i}`,
 }));
